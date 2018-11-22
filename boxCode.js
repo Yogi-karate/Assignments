4f0f58afcc84930ccfd105493d421c69dbfe6c50
Yogi-karate <simha.karate@gmail.com>
2018-11-22 17:40:41 +0530

    Box Builder complete.
    This is the box builder assignment work. Please try to keep all the three files BoxBuilder.html,boxCode.js and boxStyle.css in a folder so that the code runs smoothly without any error.

---
+++ boxCode.js
@@ -0,0 +1,74 @@
+var numList = [];
+
+function removeBox() {
+    numList.pop();
+	var myNode = document.getElementById("boxContainer");
+while (myNode.firstChild) {
+    myNode.removeChild(myNode.firstChild);
+}
+	document.getElementById('boxContainer').appendChild(makeUL(numList));
+	if(numList.length == 0)
+	{
+	document.getElementsByClassName("msg")[0].style.display = "block";
+	}
+	else
+	{
+	document.getElementsByClassName("msg")[0].style.display = "none";
+	}
+}
+function addBox() {
+    var x = numList.length + 1;
+    numList.push(x);
+	var myNode = document.getElementById("boxContainer");
+while (myNode.firstChild) {
+    myNode.removeChild(myNode.firstChild);
+}
+	document.getElementById('boxContainer').appendChild(makeUL(numList));
+	if(numList.length == 0)
+	{
+	document.getElementsByClassName("msg")[0].style.display = "block";
+	}
+	else
+	{
+	document.getElementsByClassName("msg")[0].style.display = "none";
+	}
+}
+
+function makeUL(array) {
+    // Create the list element:
+    var list = document.createElement('div');
+    for(var i = 0; i < array.length; i++) {
+        // Create the list item:
+        var item = document.createElement('p');
+        // Set its contents:
+		item.style.marginTop = "5px";
+        item.appendChild(document.createTextNode(array[i]));
+		if(i%2==0)
+		{
+        item.classList.add("even");
+		
+		}
+		else
+		{
+		item.classList.add("odd");
+		}
+		var j = i-4;
+		if(j%5==0)
+		{
+		item.classList.add("fifth");
+		}
+        // Add it to the list:
+        list.appendChild(item);
+    }
+	if(numList.length!=0)
+	{
+	var no = document.createElement('h1');
+   no.appendChild(document.createTextNode(array[array.length-1]));
+   no.style.marginTop = "-4px";
+   no.style.marginLeft = "10%";
+   list.appendChild(no);
+   }
+    // Finally, return the constructed list:
+    return list;
+}
+document.getElementById('boxContainer').appendChild(makeUL(numList));
